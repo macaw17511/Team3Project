5f4ecdae8ec96c964acc949654c7311df07f35a2
macaw17511 <carpentk11@mail.gtc.edu>
2024-11-04 18:40:41 -0600

    Add files via upload
    This is the source code

---
+++ Team3_Project/Team3_Project/AddBoat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IBM.Data.DB2.iSeries;
+
+// form to add a boat
+
+namespace Team3_Project
+{
+    public partial class AddBoat : Form
+    {
+        /////////////////////////////////////
+        //       IBM DB2 Connection 
+        /////////////////////////////////////
+        
+        iDB2Connection  conn;
+        iDB2DataAdapter adapter;
+        DataSet         dataset;
+
+
+        public AddBoat()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Team3_Project/Team3_Project/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Team3_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listAllCustomersButton = new System.Windows.Forms.Button();
+            this.listAllCountriesButton = new System.Windows.Forms.Button();
+            this.flightNumberCustomersButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.customerNumberPasswordButton = new System.Windows.Forms.Button();
+            this.resNumberCustInfoButton = new System.Windows.Forms.Button();
+            this.addCustomerButton = new System.Windows.Forms.Button();
+            this.addCountryButton = new System.Windows.Forms.Button();
+            this.addBoatButton = new System.Windows.Forms.Button();
+            this.addCarButton = new System.Windows.Forms.Button();
+            this.listRecordsButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("IBM Plex Mono", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(305, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Main Menu";
+            // 
+            // listAllCustomersButton
+            // 
+            this.listAllCustomersButton.BackColor = System.Drawing.Color.Black;
+            this.listAllCustomersButton.Font = new System.Drawing.Font("IBM Plex Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listAllCustomersButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.listAllCustomersButton.Location = new System.Drawing.Point(361, 147);
+            this.listAllCustomersButton.Name = "listAllCustomersButton";
+            this.listAllCustomersButton.Size = new System.Drawing.Size(105, 38);
+            this.listAllCustomersButton.TabIndex = 1;
+            this.listAllCustomersButton.Text = "List all Customers";
+            this.listAllCustomersButton.UseVisualStyleBackColor = false;
+            this.listAllCustomersButton.Click += new System.EventHandler(this.listAllCustomersButton_Click);
+            // 
+            // listAllCountriesButton
+            // 
+            this.listAllCountriesButton.BackColor = System.Drawing.Color.Black;
+            this.listAllCountriesButton.Font = new System.Drawing.Font("IBM Plex Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listAllCountriesButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.listAllCountriesButton.Location = new System.Drawing.Point(361, 191);
+            this.listAllCountriesButton.Name = "listAllCountriesButton";
+            this.listAllCountriesButton.Size = new System.Drawing.Size(105, 38);
+            this.listAllCountriesButton.TabIndex = 2;
+            this.listAllCountriesButton.Text = "List all Countries";
+            this.listAllCountriesButton.UseVisualStyleBackColor = false;
+            this.listAllCountriesButton.Click += new System.EventHandler(this.listAllCountriesButton_Click);
+            // 
+            // flightNumberCustomersButton
+            // 
+            this.flightNumberCustomersButton.BackColor = System.Drawing.Color.Black;
+            this.flightNumberCustomersButton.Font = new System.Drawing.Font("IBM Plex Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flightNumberCustomersButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.flightNumberCustomersButton.Location = new System.Drawing.Point(113, 147);
+            this.flightNumberCustomersButton.Name = "flightNumberCustomersButton";
+            this.flightNumberCustomersButton.Size = new System.Drawing.Size(118, 38);
+            this.flightNumberCustomersButton.TabIndex = 3;
+            this.flightNumberCustomersButton.Text = "Flight Number Customers";
+            this.flightNumberCustomersButton.UseVisualStyleBackColor = false;
+            this.flightNumberCustomersButton.Click += new System.EventHandler(this.flightNumberCustomersButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("IBM Plex Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(113, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Flight Number Seats";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // customerNumberPasswordButton
+            // 
+            this.customerNumberPasswordButton.BackColor = System.Drawing.Color.Black;
+            this.customerNumberPasswordButton.Font = new System.Drawing.Font("IBM Plex Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerNumberPasswordButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.customerNumberPasswordButton.Location = new System.Drawing.Point(113, 235);
+            this.customerNumberPasswordButton.Name = "customerNumberPasswordButton";
+            this.customerNumberPasswordButton.Size = new System.Drawing.Size(118, 38);
+            this.customerNumberPasswordButton.TabIndex = 5;
+            this.customerNumberPasswordButton.Text = "Cust. Number Password";
+            this.customerNumberPasswordButton.UseVisualStyleBackColor = false;
+            this.customerNumberPasswordButton.Click += new System.EventHandler(this.customerNumberPasswordButton_Click);
+            // 
+            // resNumberCustInfoButton
+            // 
+            this.resNumberCustInfoButton.BackColor = System.Drawing.Color.Black;
+            this.resNumberCustInfoButton.Font = new System.Drawing.Font("IBM Plex Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resNumberCustInfoButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.resNumberCustInfoButton.Location = new System.Drawing.Point(113, 279);
+            this.resNumberCustInfoButton.Name = "resNumberCustInfoButton";
+            this.resNumberCustInfoButton.Size = new System.Drawing.Size(118, 38);
+            this.resNumberCustInfoButton.TabIndex = 6;
+            this.resNumberCustInfoButton.Text = "Res. Number Cust. Info";
+            this.resNumberCustInfoButton.UseVisualStyleBackColor = false;
+            this.resNumberCustInfoButton.Click += new System.EventHandler(this.resNumberCustInfoButton_Click);
+            // 
+            // addCustomerButton
+            // 
+            this.addCustomerButton.BackColor = System.Drawing.Color.Black;
+            this.addCustomerButton.Font = new System.Drawing.Font("IBM Plex Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addCustomerButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.addCustomerButton.Location = new System.Drawing.Point(609, 147);
+            this.addCustomerButton.Name = "addCustomerButton";
+            this.addCustomerButton.Size = new System.Drawing.Size(105, 38);
+            this.addCustomerButton.TabIndex = 7;
+            this.addCustomerButton.Text = "Add a Customer";
+            this.addCustomerButton.UseVisualStyleBackColor = false;
+            this.addCustomerButton.Click += new System.EventHandler(this.addCustomerButton_Click);
+            // 
+            // addCountryButton
+            // 
+            this.addCountryButton.BackColor = System.Drawing.Color.Black;
+            this.addCountryButton.Font = new System.Drawing.Font("IBM Plex Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addCountryButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.addCountryButton.Location = new System.Drawing.Point(609, 191);
+            this.addCountryButton.Name = "addCountryButton";
+            this.addCountryButton.Size = new System.Drawing.Size(105, 38);
+            this.addCountryButton.TabIndex = 8;
+            this.addCountryButton.Text = "Add a Country";
+            this.addCountryButton.UseVisualStyleBackColor = false;
+            this.addCountryButton.Click += new System.EventHandler(this.addCountryButton_Click);
+            // 
+            // addBoatButton
+            // 
+            this.addBoatButton.BackColor = System.Drawing.Color.Black;
+            this.addBoatButton.Font = new System.Drawing.Font("IBM Plex Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addBoatButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.addBoatButton.Location = new System.Drawing.Point(609, 235);
+            this.addBoatButton.Name = "addBoatButton";
+            this.addBoatButton.Size = new System.Drawing.Size(105, 38);
+            this.addBoatButton.TabIndex = 9;
+            this.addBoatButton.Text = "Add a Boat";
+            this.addBoatButton.UseVisualStyleBackColor = false;
+            this.addBoatButton.Click += new System.EventHandler(this.addBoatButton_Click);
+            // 
+            // addCarButton
+            // 
+            this.addCarButton.BackColor = System.Drawing.Color.Black;
+            this.addCarButton.Font = new System.Drawing.Font("IBM Plex Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addCarButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.addCarButton.Location = new System.Drawing.Point(609, 279);
+            this.addCarButton.Name = "addCarButton";
+            this.addCarButton.Size = new System.Drawing.Size(105, 38);
+            this.addCarButton.TabIndex = 10;
+            this.addCarButton.Text = "Add a Car";
+            this.addCarButton.UseVisualStyleBackColor = false;
+            this.addCarButton.Click += new System.EventHandler(this.addCarButton_Click);
+            // 
+            // listRecordsButton
+            // 
+            this.listRecordsButton.BackColor = System.Drawing.Color.Black;
+            this.listRecordsButton.Font = new System.Drawing.Font("IBM Plex Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listRecordsButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.listRecordsButton.Location = new System.Drawing.Point(361, 235);
+            this.listRecordsButton.Name = "listRecordsButton";
+            this.listRecordsButton.Size = new System.Drawing.Size(105, 38);
+            this.listRecordsButton.TabIndex = 11;
+            this.listRecordsButton.Text = "List all Records";
+            this.listRecordsButton.UseVisualStyleBackColor = false;
+            this.listRecordsButton.Click += new System.EventHandler(this.listRecordsButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("IBM Plex Mono", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(328, 105);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 28);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Retrieve Info";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("IBM Plex Mono", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(124, 105);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 28);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Search";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("IBM Plex Mono", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(593, 105);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 28);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Data Entry";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(836, 485);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listRecordsButton);
+            this.Controls.Add(this.addCarButton);
+            this.Controls.Add(this.addBoatButton);
+            this.Controls.Add(this.addCountryButton);
+            this.Controls.Add(this.addCustomerButton);
+            this.Controls.Add(this.resNumberCustInfoButton);
+            this.Controls.Add(this.customerNumberPasswordButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flightNumberCustomersButton);
+            this.Controls.Add(this.listAllCountriesButton);
+            this.Controls.Add(this.listAllCustomersButton);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button listAllCustomersButton;
+        private System.Windows.Forms.Button listAllCountriesButton;
+        private System.Windows.Forms.Button flightNumberCustomersButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button customerNumberPasswordButton;
+        private System.Windows.Forms.Button resNumberCustInfoButton;
+        private System.Windows.Forms.Button addCustomerButton;
+        private System.Windows.Forms.Button addCountryButton;
+        private System.Windows.Forms.Button addBoatButton;
+        private System.Windows.Forms.Button addCarButton;
+        private System.Windows.Forms.Button listRecordsButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
