bf46b0e5ab268185e1fa45e940218ab797092b18
TheDylanBradley <bradleyd3@mail.gtc.edu>
2024-11-05 11:35:06 -0600

    Update AddCountry.cs
    Dylan Bradley 11/05/24 - Add ability to enter in a new country into the database, field lengths are controlled via the properties in the form

--- Team3_Project/Team3_Project/AddCountry.cs
+++ Team3_Project/Team3_Project/AddCountry.cs
@@ -1,4 +1,4 @@
-﻿using IBM.Data.DB2.iSeries;
+using IBM.Data.DB2.iSeries;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 // form to add a country
+
+// Dylan Bradley
 
 namespace Team3_Project
 {
@@ -28,6 +30,41 @@ namespace Team3_Project
         public AddCountry()
         {
             InitializeComponent();
+        }
+
+        
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+
+            ///////////////////////////////////////
+            //          Input Variables
+            ///////////////////////////////////////
+            
+            string Country      = textCountry.Text;
+            string Abbreviation = textAbbreviation.Text;
+
+            string cmdText = "INSERT INTO FLIGHT2024.COUNTRY VALUES(@COUNTRY, @ABBR)";
+
+            try
+            {
+                conn = new iDB2Connection("Data Source=10.250.0.30");
+                conn.Open();
+
+                iDB2Command cmd = new iDB2Command(cmdText, conn);
+                cmd.DeriveParameters();
+
+                cmd.Parameters["@COUNTRY"].Value = Country;
+                cmd.Parameters["@ABBR"].Value    = Abbreviation;
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+
+                labelError.Text = "SUCCESSFULLY ADDED TO DATABASE";
+            }
+            catch (Exception ex)
+            {
+                labelError.Text = ex.Message;
+            }
         }
     }
 }
