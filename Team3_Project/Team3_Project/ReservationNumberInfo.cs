5c52e5d07f99634708a16ea760445aadd4305978
Milchuck <milchuca1@mail.gtc.edu>
2024-11-25 18:17:20 -0600

    Update ReservationNumberInfo.cs

--- Team3_Project/Team3_Project/ReservationNumberInfo.cs
+++ Team3_Project/Team3_Project/ReservationNumberInfo.cs
@@ -1,4 +1,4 @@
-﻿using IBM.Data.DB2.iSeries;
+using IBM.Data.DB2.iSeries;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -22,12 +22,57 @@ namespace Team3_Project
 
         iDB2Connection  conn;
         iDB2DataAdapter adapter;
-        DataSet         dataset;
+        DataTable dataTable;
 
 
         public ReservationNumberInfo()
         {
             InitializeComponent();
+        }
+
+        private void ReservationNumberInfo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string UserRes = ResInputTxt.Text;
+
+            if (UserRes != "")
+            {
+                try
+                {
+                    int UserResNum = Int32.Parse(UserRes);
+                    if (UserResNum > 0 && UserResNum < 500) //replace later
+                    {
+                        ErrorLbl.Text = "Wow! It works!";
+                        // The idea of having the connection call here is so the User can try to check multiple IDs.
+                        conn = new iDB2Connection("Data Source=10.250.0.30");
+                        // SELECT [Columns] FROM [Table] WHERE [Columns]=[Desired Num]
+                        adapter = new iDB2DataAdapter("SELECT CFNAME, CLNAME, CADDR, CCITY, CSTATE, CZIP, CPHONE, CEMAIL, CDOB, CGENDER FROM FLIGHT2024.CUSTOMER WHERE CUSTNO=" + UserRes, conn);
+                        dataTable = new DataTable();
+                        dataGridView1.DataSource = dataTable;
+
+                        adapter.Fill(dataTable);
+
+                        dataGridView1.DataSource = dataTable;
+                        conn.Close();
+                    }
+                    else
+                    {
+                        ErrorLbl.Text = "Please Enter Your CustomerID Number";
+                    }
+                }
+                catch (Exception)
+                {
+                    ErrorLbl.Text = "Please Enter a Number.";
+                }
+            }
+            else
+            {
+                ErrorLbl.Text = "Please Enter a Number.";
+            }
         }
     }
 }
